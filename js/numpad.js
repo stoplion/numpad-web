f8c5cb8eb8b2ecdf43ec25956bbb874cc53b46df
Timur Atalay <AIUXW77@amway.com>
2019-10-31 14:01:00 -0700

    Fix merge conflict

--- js/numpad.js
+++ js/numpad.js
@@ -35,155 +35,7 @@ const appSettings = () => db.get('settings') || (db.set('settings', appDefaults)
 
 (() => {
     const appName = 'Numpad';
-    const appVersion = '1.0.2';
-
-    const calculate = () => {
-        var settings = appSettings();
-        var solve = math.evaluate;
-        var input = $('input').value;
-        var lines = input.split('\n');
-        var lineIndex = 1;
-        var lineNos = [];
-        var scrolls = [];
-        var answers = [];
-        var totals = [];
-        var scope = {};
-        var expLim = {
-            lowerExp: -12,
-            upperExp: 12
-        };
-        var digits = {
-            maximumFractionDigits: settings.precision
-        };
-
-        scope.now = moment().format(settings.dateFormat + ' LT');
-        scope.today = moment().format(settings.dateFormat);
-
-        for (var i in lines) {
-            var line = lines[i].trim();
-            var lineNo = lineIndex++;
-            var answer = '';
-
-            if (line) {
-                try {
-                    line = lineNo > 1 && line.charAt(0).match(/[\+\-\*\/]/) && lines[i - 1].length > 0 ? scope.ans + line : line;
-                    try {
-                        answer = solve(line, scope);
-                    } catch (e) {
-                        while (line.match(/\([^\)]+\)/)) {
-                            var s = line.substring(line.lastIndexOf('(') + 1);
-                            var sp = line.substring(line.lastIndexOf('('));
-
-                            s = s.substring(0, s.indexOf(')'));
-                            sp = sp.substring(0, sp.indexOf(')') + 1);
-
-                            if (sp.length === 0) break;
-
-                            try {
-                                line = line.replace(sp, solver(s));
-                            } catch (e) {
-                                break;
-                            }
-                        }
-                        answer = solver(line);
-                    }
-
-                    if (answer !== undefined) {
-                        scope.ans = scope['line' + lineNo] = answer;
-                        answer = math.format(answer, expLim);
-
-                        var a = answer.trim().split(' ')[0];
-                        var b = answer.replace(a, '');
-                        answer = !a.includes('e') && !isNaN(a) ? Number(a).toLocaleString(undefined, digits) + b : strip(answer);
-
-                        if (!isNaN(a)) totals.push(Number(a));
-
-                        scope.total = totals.reduce((a, b) => a + b, 0);
-
-                        if (answer.match(/\w\(x\)/)) {
-                            answer = '<a title="Plot ' + line + '" class="plotButton" data-func="' + line + '" uk-tooltip>Plot</a>';
-                            scope.ans = scope['line' + lineNo] = line.split('=')[1].trim();
-                        }
-                    } else {
-                        answer = '';
-                    }
-                } catch (e) {
-                    var errStr = String(e).replace(/'|"/g, '`');
-                    if (settings.lineErrors) {
-                        answer = '<a title="' + errStr + '" class="lineError" data-line="' + lineNo + '" data-error="' + errStr + '" uk-tooltip>err</a>';
-                        lineNo = '<span class="lineErrorNo">' + lineNo + '</span>';
-                    }
-                }
-            }
-
-            answers += answer + '<br>';
-            lineNos += lineNo + '<br>';
-            scrolls += '<br>';
-        }
-
-        $('lineNo').innerHTML = lineNos;
-        $('output').innerHTML = answers;
-        $('scroll').innerHTML = scrolls;
-
-        $('clearButton').className = input === '' ? 'noAction' : 'action';
-        $('printButton').className = input === '' ? 'noAction' : 'action';
-        $('saveButton').className = input === '' ? 'noAction' : 'action';
-
-        db.set('input', $('input').value);
-        $('undoButton').style.visibility = 'hidden';
-
-        function strip(s) {
-            var t = s.length;
-            if (s.charAt(0) === '"') s = s.substring(1, t--);
-            if (s.charAt(--t) === '"') s = s.substring(0, t);
-            return s;
-        }
-
-        // Solver
-        function solver(line) {
-            line = line.replace(/\bans\b/g, scope.ans);
-            line = line.replace(/\bnow\b/g, scope.now);
-            line = line.replace(/\btoday\b/g, scope.today);
-
-            var lineNoReg = line.match(/\bline\d+\b/g);
-            if (lineNoReg) lineNoReg.map(n => line = line.replace(n, scope[n]));
-
-            var timeReg = 'hour|hours|minute|minutes|second|seconds';
-            var dateReg = 'day|days|week|weeks|month|months|year|years|' + timeReg;
-            var momentReg = new RegExp('[\\+\\-]\\s*\\d*\\s*(' + dateReg + ')\\s*$');
-
-            if (line.match(momentReg)) {
-                var lineDate = line.split(/[\+\-]/)[0];
-                var rightOfDate = String(solve(line.replace(lineDate, ''), scope));
-                var dwmy = rightOfDate.match(new RegExp(dateReg));
-                var dateNum = rightOfDate.split(dwmy)[0];
-                var timeFormat = line.match(new RegExp(timeReg)) ? settings.dateFormat + ' LT' : settings.dateFormat;
-
-                line = '"' + moment(new Date(lineDate)).add(dateNum, dwmy).format(timeFormat) + '"';
-            }
-
-            var modReg = /\d*\.?\d%\d*\.?\d/g;
-            var pcntReg = /[\w.]*%/g;
-            var pcntOfReg = /%[ ]*of[ ]*/g;
-            var pcntOfRegC = /[\w.]*%[ ]*of[ ]*/g;
-
-            line = line.match(pcntOfRegC) ? line.replace(pcntOfReg, '/100*') : line;
-
-            if (line.match(modReg)) line.match(modReg).map(m => line = line.replace(m, solve(m, scope)));
-
-            while (line.match(pcntReg) && !line.match(modReg)) {
-                var right = line.match(pcntReg)[0];
-                var rightVal = solve(right.slice(0, -1), scope);
-                var left = line.split(right)[0];
-                var leftVal = solve(left.trim().slice(0, -1), scope);
-
-                newval = solve(leftVal + '*' + rightVal + '/100', scope);
-                line = line.replace(left + right, solve(left + newval, scope));
-            }
-
-            return solve(line, scope);
-        }
-    };
+    const appVersion = '1.2.0';
 
     var settings;
 
@@ -246,6 +98,9 @@ const appSettings = () => db.get('settings') || (db.set('settings', appDefaults)
         $('input').value = db.get('input');
     }
 
+    // Load last calculations
+    $('input').value = db.get('input');
+
     // Apply settings
     applySettings();
 
@@ -253,8 +108,8 @@ const appSettings = () => db.get('settings') || (db.set('settings', appDefaults)
         settings = appSettings();
         $('lineNo').style.display = settings.lineNumbers ? 'block' : 'none';
         $('handle').style.display = settings.resizable ? 'block' : 'none';
-        $('inputCol').style.width = settings.resizable ? settings.inputWidth : '50%';
-        $('inputCol').style.marginLeft = settings.lineNumbers ? '0px' : '18px';
+        $('inputPane').style.width = settings.resizable ? settings.inputWidth : '50%';
+        $('inputPane').style.marginLeft = settings.lineNumbers ? '0px' : '18px';
         $('output').style.textAlign = settings.resizable ? 'left' : 'right';
 
         $("wrapper").style.visibility = 'visible';
@@ -341,8 +196,8 @@ const appSettings = () => db.get('settings') || (db.set('settings', appDefaults)
                         $('printLines').innerHTML = $('lineNo').innerHTML;
                         $('printInput').innerHTML = $('input').value;
                         $('printOutput').innerHTML = $('output').innerHTML;
-                        ipcRenderer.once('actionReply', (event, response) => showMsg(response));
-                        ipcRenderer.send('print');
+                        ipc.send('print');
+                        ipc.once('printReply', (event, response) => showMsg(response));
                     }
                     break;
                 case 'saveButton': // Save calcualtions
@@ -440,10 +295,7 @@ const appSettings = () => db.get('settings') || (db.set('settings', appDefaults)
                     });
                     break;
                 case 'dialog-settings-reset': // Reset app
-                    confirm('All user settings and data will be lost.', () => {
-                        localStorage.clear();
-                        location.reload();
-                    });
+                    confirm('All user settings and data will be lost.', () => ipc.send('resetApp'));
                     break;
                 case 'updateRatesButton': // Update exchange rates
                     getRates();
@@ -480,7 +332,7 @@ const appSettings = () => db.get('settings') || (db.set('settings', appDefaults)
                 case 'reset-plotRange': // Reset plot range
                     confirm('Reset plot range to defaults?', () => {
                         var plotRange = settings.plotRange;
-                        Object.keys(settings.plotRange).map(key => plotRange[key] = appDefaults.plotRange[key]);
+                        Object.keys(plotRange).map(key => plotRange[key] = appDefaults.plotRange[key]);
                         db.set('settings', settings);
                         UIkit.modal('#dialog-plotRange').hide();
                         plot();
@@ -521,14 +373,15 @@ const appSettings = () => db.get('settings') || (db.set('settings', appDefaults)
             if (savedItems.length > 0) {
                 $('dialog-open-deleteAll').disabled = false;
                 savedItems.map(id => {
-                    $('dialog-open-body').innerHTML +=
+                    $('dialog-open-body').innerHTML += (
                         '<div class="dialog-open-wrapper" id="' + id + '">' +
                         '<div data-action="load">' +
                         '<div class="dialog-open-title">' + obj[id][0] + '</div>' +
                         '<div class="dialog-open-date">' + moment(Number(id)).format('lll') + '</div>' +
                         '</div>' +
                         '<div class="dialog-open-delete" data-action="delete">&#10005;</div>' +
-                        '</div>';
+                        '</div>'
+                    );
                 });
             } else {
                 $('dialog-open-deleteAll').disabled = true;
@@ -656,7 +509,7 @@ const appSettings = () => db.get('settings') || (db.set('settings', appDefaults)
 
         // Show confirmation dialog
         function confirm(msg, action) {
-            UIkit.util.on("#dialog-confirm", 'beforeshow', () => $('confirmMsg').innerHTML = msg);
+            $('confirmMsg').innerHTML = msg;
             UIkit.modal('#dialog-confirm').show();
             var yesAction = (e) => {
                 action();
@@ -665,6 +518,7 @@ const appSettings = () => db.get('settings') || (db.set('settings', appDefaults)
                 $('confirm-yes').removeEventListener('click', yesAction);
             };
             $('confirm-yes').addEventListener('click', yesAction);
+            UIkit.util.on("#dialog-confirm", 'hidden', () => $('confirm-yes').removeEventListener('click', yesAction));
         }
 
         // Show error dialog
@@ -680,12 +534,8 @@ const appSettings = () => db.get('settings') || (db.set('settings', appDefaults)
         function showMsg(msg) {
             $('msg').innerHTML = msg;
             $('msg').style.opacity = '1';
-            setTimeout(() => {
-                $('msg').style.opacity = '0';
-            }, 2000);
-            setTimeout(() => {
-                $('msg').innerHTML = '';
-            }, 2300);
+            setTimeout(() => $('msg').style.opacity = '0', 2000);
+            setTimeout(() => $('msg').innerHTML = '', 2300);
         }
 
         /**
@@ -701,7 +551,7 @@ const appSettings = () => db.get('settings') || (db.set('settings', appDefaults)
         (() => {
             var names = {};
             var scroll = () => {
-                var elems = document.getElementsByClassName('syncscroll');
+                var elems = document.getElementsByName('sync');
                 var i, j, el, found, name;
                 var scrollSync = (el, name) => {
                     el.addEventListener('scroll', el.syn = () => {
